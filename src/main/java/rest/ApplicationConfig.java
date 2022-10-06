7cecafadc24adc0f9f04cad1c6162ed7d9d728dc
thomasfritzboger <cph-tf67@cphbusiness.dk>
2022-10-06 10:27:56 +0200

    add cors to application config

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -28,6 +28,7 @@ public class ApplicationConfig extends Application {
         resources.add(rest.PersonResource.class);
         resources.add(rest.HobbyResource.class);
         resources.add(rest.CityInfoResource.class);
+        resources.add(rest.CorsFilter.class);
     }
     
 }
